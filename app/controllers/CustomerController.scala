9f4ec652ef73b5e7739cb7b7771ad44993e182cb
afshikh2001 <afshikh2001@yahoo.com>
2018-09-27 12:35:10 +0200

    bug has been fixed

--- app/controllers/CustomerController.scala
+++ app/controllers/CustomerController.scala
@@ -2,8 +2,9 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
-import request.CustomerData
-import request.CustomerData.customerDataForm
+import request.CustomerLoginForm
+import request.CustomerLoginForm.customerLoginForm
+import request.CustomerRegistrationForm.customerRegistrationForm
 
 
 @Singleton
@@ -13,13 +14,14 @@ class CustomerController @Inject()(cc: ControllerComponents) extends AbstractCon
     Ok(views.html.login(""))
   }
 
-  def signin() = Action { implicit request: Request[AnyContent] =>
-
+  def signin() = Action { implicit request =>
+    val loginData = customerLoginForm.bindFromRequest.get
+    println(loginData.toString)
     Ok(views.html.login(""))
   }
 
-  def signup() = Action(parse.form(customerDataForm)) { implicit request =>
-    val customerData = customerDataForm.bindFromRequest().get
+  def signup() = Action { implicit request =>
+    val customerData = customerRegistrationForm.bindFromRequest().get
     println(customerData.toString)
     Ok(views.html.login(""))
   }
